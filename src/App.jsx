1f127a6151e8b79e2da3bc9e69885730120e7336
Igor-Jacinto <telesigornm@gmail.com>
2022-07-03 20:57:23 -0400

    botao-e-estilizacao

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,133 @@ import Botao from "./Botao";
 function App(){
 
 		const botao = [
-			
+			{
+				title: "linux",
+			},
+			{
+				title: "docker",
+			},
+			{
+				title: "server",
+			},
+			{
+				title: "rails",
+			},
+			{
+				title: "douane",
+			},
+			{
+				title: "my-developments",
+			},
+			{
+				title: "command-line",
+			},
+			{
+				title: "ruby",
+			},
+			{
+				title: "c-2",
+			},
+			{
+				title: "linux-on-mac",
+			},
+			{
+				title: "maintenance",
+			},
+			{
+				title: "security",
+			},
+			{
+				title: "gnome",
+			},
+			{
+				title: "kubernetes",
+			},
+			{
+				title: "mes-developpements",
+			},
+			{
+				title: "ubuntu",
+			},
+			{
+				title: "python",
+			},
+			{
+				title: "vcs",
+			},
+			{
+				title: "capybara",
+			},
+			{
+				title: "contribution",
+			},
+			{
+				title: "git",
+			},
+			{
+				title: "apt",
+			},
+			{
+				title: "bazaar",
+			},
+			{
+				title: "chef",
+			},
+			{
+				title: "cucumber",
+			},
+			{
+				title: "debian",
+			},
+			{
+				title: "game",
+			},
+			{
+				title: "howto",
+			},
+			{
+				title: "packaging",
+			},
+			{
+				title: "testing",
+			},
+			{
+				title: "boost",
+			},
+			{
+				title: "debug",
+			},
+			{
+				title: "devise",
+			},
+			{
+				title: "elixir",
+			},
+			{
+				title: "gtk",
+			},
+			{
+				title: "homebrew",
+			},
+			{
+				title: "information",
+			},
+			{
+				title: "ssh",
+			},
+			{
+				title: "tuxtremsplit",
+			},
+			{
+				title: "xpath",
+			},
 		];
 
 	return(
 
 		<div className="cont"> 
 			{
-			palavras.map((cons, cont) => {
+			botao.map((cons, cont) => {
 					return <Botao key={cont}
 							title={cons.title}
 						/>;
